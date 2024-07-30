b0beee7da54794ac0f57a413b95bd1dc2869c2dc
Hiteshwar <hemant.kaimwal96@gmail.com>
2024-07-30 17:25:52 +0530

    project is completed successfully

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ var typed = new Typed('#element', {
 
 
 
+// window.addEventListener('load', () => {
+//     document.querySelectorAll('.skills-progress').forEach(skill => {
+//       skill.style.width = '100%';
+//     });
+//   });
+
+
+// const skillSection = document.querySelector('.skills-display'); // Replace with your actual section ID
+// const progress = document.querySelector('#progress'); 
+// const html = document.querySelector('#html'); 
+
+// console.log(progress);
+// console.log("before",html);
+// html.classList.remove('html-skill')
+// console.log("after",html);
+
+
+// console.log(skillSection.getBoundingClientRect);
+// window.addEventListener('scroll', () => {
+//   const rect = skillSection.getBoundingClientRect();
+//   console.log(rect);
+//   if (rect.top < window.innerHeight) {
+//     // Skill section is in view, start animation
+//     html.style.width = '100%';
+//   }
+// });
